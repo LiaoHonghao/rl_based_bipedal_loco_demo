d69986a2ba1ed52e6a85f9a5822120c4d5dc9b5b
liao <19862181387@163.com>
2024-08-04 14:18:42 +0800

    initial commit

---
+++ src/types/types.d.ts
@@ -0,0 +1,20 @@
+type Author = {
+  name: string;
+  url?: string;
+  institution?: string;
+  mail?: string;
+  notes?: string[];
+}
+
+type Link = {
+  url: string;
+  name: string;
+  icon?: string;
+}
+
+type Note = {
+  symbol: string;
+  text: string;
+}
+
+export { Author, Link, Note };
